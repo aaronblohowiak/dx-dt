3ddcc162779a5ea824c5dbfc5423e1e69f6a04d1
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2011-08-30 00:24:28 -0700

    accept parameterized output directory, minimize machine id,

--- agent/collect.sh
+++ agent/collect.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-now="stats_`date +%s`"
+if [ -n "$1" ]
+then infodir="$1";
+else infodir="stats_`date +%s`"; 
+fi
 
-process_dir="$now/processes"
-sys_dir="$now/sys"
+process_dir="$infodir/processes"
+sys_dir="$infodir/sys"
 
-mkdir $now
+mkdir -p $infodir
 mkdir $process_dir
 mkdir $sys_dir
 
 if command -v md5 &>/dev/null; 
-  then machine_id=`ifconfig -a | grep ..:..:..:..:..:.. | md5`; 
-  else machine_id=`ifconfig -a | grep ..:..:..:..:..:.. | md5sum`; 
-fi 
+  then machine_id=`ifconfig -a | grep ..:..:..:..:..:.. | md5 | grep '\w*' -o`; 
+  else machine_id=`ifconfig -a | grep ..:..:..:..:..:.. | md5sum | grep '\w*' -o`; 
+fi
 
-echo $machine_id > $now/machineid
+echo $machine_id > $infodir/machineid
 
 pslist="ps -A -o pid -o"
 
 function getInfo {
   $pslist $1 > $process_dir/$1.stdout.txt &2> $process_dir/$1.stderr.txt
 }
-
 
 function getInfoPercent {
   $pslist %$1 > $process_dir/$1.stdout.txt &2> $process_dir/$1.stderr.txt
@@ -35,6 +37,15 @@ getInfo utime
 getInfoPercent cpu
 getInfoPercent mem
 
-uptime > $sys_dir/uptime.txt
+if command -v vm_stat &>/dev/null;
+  then vm_stat > $sys_dir/vm_stat.stdout.txt &2> $sys_dir/vmstat.stderr.txt;
+  else vmstat > $sys_dir/vmstat.stdout.txt &2> $sys_dir/vmstat.stderr.txt;
+fi
 
-tar -cf $now.tar $now/
+uptime > $sys_dir/uptime.txt
+df -k > $sys_dir/df.txt
+echo `date` > $sys_dir/date
+
+tar -czf $infodir.tgz $infodir/
+rm -rf $infodir/
+
