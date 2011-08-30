53a94e8c05c061670d98a39bb6f6d842a7b8e3f6
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2011-08-30 01:33:48 -0700

    much improved agent

--- agent/collect.sh
+++ agent/collect.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+###############################################################
+#   __          __  ___
+#  |  \ \_/ __ |  \  |
+#  |__/ / \    |__/  |
+#
+###############################################################
+# Machine-parseable process monitoring
+###############################################################
+
+
+
+###############################################################
+# Begin critical section; exit if any line returns anything bad
+###############################################################
+set -e
+###############################################################
+
 if [ -n "$1" ]
 then infodir="$1";
 else infodir="stats_`date +%s`"; 
@@ -12,40 +29,91 @@ mkdir -p $infodir
 mkdir $process_dir
 mkdir $sys_dir
 
+
 if command -v md5 &>/dev/null; 
-  then machine_id=`ifconfig -a | grep ..:..:..:..:..:.. | md5 | grep '\w*' -o`; 
-  else machine_id=`ifconfig -a | grep ..:..:..:..:..:.. | md5sum | grep '\w*' -o`; 
+  then hshcommand="md5"; #BSD
+  else hshcommand="md5sum"; #LINUX
 fi
 
-echo $machine_id > $infodir/machineid
+if command -v vm_stat &>/dev/null;
+  then  vmcommand="vm_stat"; #OSX/BSD 
+  else  vmcommand="vmstat"; #LINUX
+fi
 
+
+
+# Generic Run a command and save stdout and stderr to files
+
+function runAndRecord {
+  # command directory basename
+  $1 > "$2/$3.stdout.txt" &2> "$2/$3.stderr.txt"
+}
+
+function log {
+  # command directory
+  runAndRecord "$1" "$2" "$1"
+}
+
+
+# Process monitoring functions
+function runAndRecordProcess {
+  # command basename
+  runAndRecord "$1" "$process_dir" "$2"
+}
+
+# Utility function to get info about all the processes on the machine
+# Note that we have to do this with multiple calls to ps because the output of `ps` is otherwise not machine parseable
 pslist="ps -A -o pid -o"
-
 function getInfo {
-  $pslist $1 > $process_dir/$1.stdout.txt &2> $process_dir/$1.stderr.txt
+  # ps format string
+  runAndRecordProcess "$pslist $1" $1
 }
 
 function getInfoPercent {
-  $pslist %$1 > $process_dir/$1.stdout.txt &2> $process_dir/$1.stderr.txt
+  # ps format string that will be prepended with %
+  runAndRecordProcess "$pslist %$1" $1
 }
 
-getInfo lstart
-getInfo args
-getInfo vsz
-getInfo utime
+###############################################################
+# END critical section; exit if any line returns anything bad
+###############################################################
+set +e
+###############################################################
+
+
+getInfo lstart #Full start time
+getInfo args # full command name with args (unless ps is in "safe mode")
+getInfo vsz # virtual size in Kbytes
+getInfo utime # user CPU time
+getInfo time # user + system CPU time
 
 getInfoPercent cpu
 getInfoPercent mem
 
-if command -v vm_stat &>/dev/null;
-  then vm_stat > $sys_dir/vm_stat.stdout.txt &2> $sys_dir/vmstat.stderr.txt;
-  else vmstat > $sys_dir/vmstat.stdout.txt &2> $sys_dir/vmstat.stderr.txt;
-fi
+#System-wide performance
+log $vmcommand "$sys_dir"
+log uptime "$sys_dir"
+log date "$sys_dir"
+runAndRecord "df -k" "$sys_dir" df
 
-uptime > $sys_dir/uptime.txt
-df -k > $sys_dir/df.txt
-echo `date` > $sys_dir/date
+###############################################################
+# Begin critical section; exit if any line returns anything bad
+###############################################################
+set -e
+###############################################################
 
-tar -czf $infodir.tgz $infodir/
+#Network info
+log "hostname" "$sys_dir"
+net_configuration=`ifconfig -a`
+echo "$net_configuration" > $sys_dir/ifconfig.txt
+
+#generate machine id from everything that looks like a MAC addresses present in network info
+machine_id=`echo "$net_configuration" |  grep ..:..:..:..:..:.. | $hshcommand | grep  '\w*' -o`
+echo "$machine_id" > $infodir/machineid
+
+outputfilename="$infodir.tgz"
+tar -czf "$outputfilename" $infodir/
 rm -rf $infodir/
+
+
 
