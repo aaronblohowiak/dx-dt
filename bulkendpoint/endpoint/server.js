1fbaf1959c827a69f21b478cd3391a4cca214c2c
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2011-09-11 15:31:42 -0700

    moving on up!

--- bulkendpoint/endpoint/server.js
+++ bulkendpoint/endpoint/server.js
@@ -7,4 +7,23 @@ var options = {port:8081};
   Transitive.boot(this, options);
 })();
 
+
+if(! ("app" in Transitive)){
+  Transitive.app = {};
+}
+
+// if(Transitive.options.useS3){
+//   Transitive.app.blobStorage = require('noxmox').nox.createClient({
+//     key: '<api-key-here>',
+//     secret: '<secret-here>',
+//     bucket: 'mybucket'
+//   });  
+// } else {
+//   Transitive.app.blobStorage = require('noxmox').mox.createClient({
+//     key: '<api-key-here>',
+//     secret: '<secret-here>',
+//     bucket: 'mybucket'
+//   });
+// }
+
 console.log("Server started. listening on port " + Transitive.server.address().port);
