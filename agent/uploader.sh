8f0cb9b5fe2391819f3829332cbe8a814bf78b54
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2011-09-11 23:59:45 -0700

    fixed uploader

--- agent/uploader.sh
+++ agent/uploader.sh
@@ -5,19 +5,23 @@ if [ -n "$DXDT_ENDPOINT"]; then
   DXDT_ENDPOINT=http://localhost:8081/;
 fi
 
+if [ -n "$DXDT_TOKEN"]; then
+  DXDT_TOKEN=NO_TOKEN;
+fi
+
+
 for filename in *.tgz; do
   set +e
 
-  code=`curl -F "bulk=@$filename" -F "token=$DXDT_TOKEN&environment=$DXDT_ENVIRONMENT" --write-out %{http_code} --silent --output /dev/null $DXDT_ENDPOINT`;
+  code=`curl -F "bulk=@$filename" -F "token=$DXDT_TOKEN" -F"environment=$DXDT_ENVIRONMENT" --write-out %{http_code} --silent --output /dev/null $DXDT_ENDPOINT`;
   set -e
 
   echo $code
 
   if [ "$code" == "200" ]; then
-    echo "would delete" $filename;
+    echo "would rm $filename";
   else
-    echo "would not delete it";
+    echo "upload of file failed, will not delete.";
   fi
-  exit
 done
 
