efbe8a2957913b59cf5f4d9fabd8cfc794b19f40
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2011-11-07 16:44:58 -0800

    add basic auth to bulk endpoint

--- endpoint/controllers/home.js
+++ endpoint/controllers/home.js
@@ -1,15 +1,39 @@
 formidable =  require("formidable");
 msgpack = require("msgpack2");
 newId = require("uuid-pure").newId;
+basicAuth = require("../lib/basic-auth");
 
 module.exports = function(routes, Transitive){
-  var q = Transitive.app.bulkQueue;
+  var q = Transitive.App.bulkQueue;
+
+  routes.post("/install-failed", function(req, res){
+    var auth = basicAuth(req);
+    var account = auth[0], token = auth[1];
+    
+    //todo: check authentication credentials.
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+      q.redis.publish("/pushIt/accounts/"+account+"/installer", JSON.stringify({
+        type:"failed",
+        reason: fields.failure
+      }));
+      res.writeHead(200,{});
+      res.end("Failure has been reported. Thank you.");
+    });
+  });
 
   routes.post("/", function(req, res){
+    var auth = basicAuth(req);
+    var account = auth[0], token = auth[1];
+    
     var form = new formidable.IncomingForm();
     form.uploadDir = Transitive.options.root + '/tmp';
     
      form.parse(req, function(err, fields, files) {
+       fields.account = account;
+       fields.token = token;
+
        var id = newId();
        if(err){
          res.writeHead(500, {'content-type': 'text/plain'});
@@ -18,7 +42,7 @@ module.exports = function(routes, Transitive){
          return;
        }
 
-       var shallowFiles = {}, f;
+       var shallowFiles = {}, f; // can't serialize files normally =/
        for(var filename in files){
          f = files[filename];
          shallowFiles[filename] = {
